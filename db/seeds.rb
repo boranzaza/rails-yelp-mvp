076b2108a6245f54c175a8c73f092f79f802742f
Boran Zaza <boran.m.zaza@gmail.com>
2020-08-12 16:55:39 -0400

    seeds now working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,21 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-require "faker"
-# TODO: Write a seed to insert 100 posts in the database
 
-5.times do
-  restaurant = Restaurant.new(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.full_address,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    category: "chinese"
-  )
-  post.save
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chez_claudette = { name: 'Chez Claudette', address: 'Laurrier 123', phone_number: '438-999-9911', category: 'french' }
+chez_boran = { name: 'Chez Boran', address: 'Laval 123', phone_number: '438-995-9911', category: 'italian' }
+chez_micha = { name: 'Chez Micha', address: 'Laval 123', phone_number: '438-699-9911', category: 'belgian' }
+chez_miguel = { name: 'Chez Miguel', address: 'Papineau 123', phone_number: '438-799-9911', category: 'chinese' }
+chez_erika = { name: 'Chez Erika', address: 'Brossard 123', phone_number: '438-929-9911', category: 'japanese' }
+
+[chez_claudette, chez_boran, chez_micha, chez_miguel, chez_erika].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
 end
-
+puts "Finished!"
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
